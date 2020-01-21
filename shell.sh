03f7196dad72a3496a89c64019827a21da5ec5fc
mineshmane <mineshmane94@gmail.com>
2020-01-21 11:26:31 +0530

    Refactor:USECASE Employee is Present

--- shell.sh
+++ shell.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 	echo welcome to employee wage
 	echo  welcome to in attendace usecadse
+isPresent=1;
+isAbsent=0;
+attendance=$((RANDOM%2))
+	if (( isPresent == attendance ))
+	then
+		echo EMployee Present
+	else
+		echo Employee is absent 
+	fi
+
