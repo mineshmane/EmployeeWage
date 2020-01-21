2bbacf3cd593aa0bd07aefbbb70752c7e89db585
mineshmane <mineshmane94@gmail.com>
2020-01-21 12:23:36 +0530

    Added:USE_CASE2 added part time Employee

--- shell.sh
+++ shell.sh
@@ -5,12 +5,22 @@ isPresent=1;
 isAbsent=0;
 RATE=20;
 wgaes=0;
-fullTime=8;
+PART_TIME=0;
+FULL_TIME=1;
+FULL_TIME_HR=8;
+PART_TIME_HR=4;
 attendance=$((RANDOM%2))
-	if (( isPresent == attendance ))
+employeeType=$((RANDOM%2))
+	if (( isPresent == attendance  ))
 	then
-			wages=$(( $RATE * $fullTime ))
-		echo  "total Wages" $wages
+		if [ $employeeType ==  $FULL_TIME  ] 
+		then
+			wages=$(( $RATE * $FULL_TIME_HR ))
+			echo  "Employee Full Time  Wages" $wages
+		else
+			wages=$(( $RATE * $PART_TIME_HR ))
+			echo " PArtime Employee " $wagesxy
+		fi	
 	else
 		echo "Employee is absent wages is zero" $wages
 	fi
