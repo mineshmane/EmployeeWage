c3e139c5cbea442c653a257a61d7bae44c6ce42d
mineshmane <mineshmane94@gmail.com>
2020-01-21 16:55:49 +0530

    Added:UseCase5 condtion of hours to calculate month wage

--- shell.sh
+++ shell.sh
@@ -11,7 +11,6 @@ NUM_WORKING_DAYS=20
 #Variables
 totalEmpHr=0
 totalWorkingDays=0
-
 function getWorkingHours(){
 		case $1 in 
 			$FULL_TIME)
@@ -23,16 +22,14 @@ function getWorkingHours(){
 			$isAbsent)
 				workHours=0
 				;;
-
 		esac
-			echo $workHours
+				echo $workHours
 	}
-
-		while [[ $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+		while [[ $totalWorkHours -lt 100 && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 		do
 			((totalWorkingDays++))
 			workHours="$( getWorkingHours $((RANDOM%3)) )"	
 			totalWorkHours=$(( $totalWorkHours + $workHours ))
 		done
-			echo  "Employee Full Time  Wages" totalSalary=$(( $totalWorkHours*$RATE ))
+			echo  "Employee full Month wages " totalSalary=$(( $totalWorkHours*$RATE ))
 
